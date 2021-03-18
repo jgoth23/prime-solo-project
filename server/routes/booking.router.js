55c5eaa9a0e34f66df6861dc88404cb16dc3e22e
Johnny Goth <Jgoth23@gmail.com>
2021-03-18 00:24:04 -0500

    get is set

--- server/routes/booking.router.js
+++ server/routes/booking.router.js
@@ -2,6 +2,19 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+
+router.get('/', (req, res) => {
+  const queryText = `SELECT * FROM booking WHERE booking.user_id = $1`
+  pool.query(queryText, [req.user.id])
+    .then((result) => {
+      res.send(result.rows)
+    })
+    .catch((err) => {
+      console.log('get error', err);
+      res.sendStatus(500);
+    })
+});
+
 router.post('/', (req, res) => {
   const newSession = req.body;
   console.log('req.user', req.user);
